c9990412b64a0d134fe266b01aac186ed907ca38
David Vorona <davidavorona@gmail.com>
2021-01-31 15:38:38 -0700

    Parse argument text and extract tokens

--- bot.js
+++ bot.js
@@ -62,10 +62,99 @@ const sendUpdateMessage = (channel, text, name, args) => {
     });
 };
 
-const parseCommand = (text) => {
-    const args = text.toLowerCase().split(' ').filter((arg) => {
+const extractArgs = (text) => {
+    // eslint-disable-next-line quotes
+    const SINGLE_QUOTE_CHAR = `'`;
+    const DOUBLE_QUOTE_CHAR = '"';
+    const TICK_CHAR = '`';
+    const SPACE_CHAR = ' ';
+    const args = [];
+    // remove start/end, extra whitespace
+    const chars = text.trim().replace(/\s+/g, ' ').toLowerCase().split('');
+    let i = 0;
+    while (i < chars.length) {
+        const c = chars[i];
+        if (i === 0 && c !== SPACE_CHAR) {
+            // find first space (cannot be first character)
+            const j = chars.indexOf(SPACE_CHAR);
+            // if no space, assume no arguments and break
+            if (j === -1) {
+                // first token is characters before end
+                const firstToken = chars.slice(i).join('');
+                args.push(firstToken);
+                break;
+            // otherwise, find first token and continue
+            } else {
+                // first token is characters before first space
+                const firstToken = chars.slice(i, j).join('');
+                args.push(firstToken);
+                i = j;
+            }
+        } else if (c === SPACE_CHAR) {
+            // find index of first char after space
+            const tokenStart = i + 1;
+            // increment i and step forward if next char is quote
+            if (
+                chars[tokenStart] === SINGLE_QUOTE_CHAR
+                || chars[tokenStart] === DOUBLE_QUOTE_CHAR
+                || chars[tokenStart] === TICK_CHAR
+            ) {
+                i = tokenStart;
+            // otherwise, find token after space
+            } else {
+                // find index of next space
+                const j = chars.indexOf(c, tokenStart);
+                if (j === -1) {
+                    // if no space, end of text
+                    const token = chars.slice(tokenStart).join('');
+                    args.push(token);
+                    break;
+                } else {
+                    // otherwise, token is chars up to next space 
+                    const token = chars.slice(tokenStart, j).join('');
+                    args.push(token);
+                    i = j;   
+                }                
+            }
+        } else if (
+            c === SINGLE_QUOTE_CHAR
+            || c === DOUBLE_QUOTE_CHAR
+            || c === TICK_CHAR
+        ) {
+            // find index of first char after quote
+            const tokenStart = i + 1;
+            // find index of next quote
+            const j = chars.indexOf(c, tokenStart);
+            if (j === -1) {
+                // if no quote, end of text
+                const token = chars.slice(tokenStart).join('');
+                args.push(token);
+                break;
+            } else {
+                // otherwise, token is chars up to next quote 
+                let token = chars.slice(tokenStart, j).join('');
+                // trim token b/c above regex does not remove
+                // start/end space in quoted text
+                token = token.trim();
+                args.push(token);
+                i = j;   
+            }       
+        } else {
+            i += 1;
+        }
+        // ugly check to prevent infinite loops
+        if (i === -1) {
+            break;
+        }
+    }
+    // remove bot mention from args
+    return args.filter((arg) => {
         return arg && arg.indexOf('@') === -1;
     });
+};
+
+const parseCommand = (text) => {
+    const args = extractArgs(text);
     return {
         text,
         command: args[0],
@@ -461,7 +550,7 @@ const commands = {
                 const messageText = `**${player}** has killed **${bossName}** **${killCounts[bossID]}** times`;
                 sendUpdateMessage(msg.channel, messageText, bossID, {
                     title: bossName,
-                    url: `${constants.osrsWikiBaseUrl}${bossName}`
+                    url: `${constants.osrsWikiBaseUrl}${encodeURIComponent(bossName)}`
                 });
             }).catch((err) => {
                 log.push(`Error while fetching hiscores (check) for player ${player}: ${err.toString()}`);
