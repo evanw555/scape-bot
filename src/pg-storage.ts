3aeeb8f5839e5675f15f387ca2d89339042a7fee
Evan Williams <evanw555@gmail.com>
2022-10-08 13:05:57 -0700

    Use pg-format to insert multiple rows in one query

--- src/pg-storage.ts
+++ src/pg-storage.ts
@@ -1,8 +1,9 @@
 import { Snowflake } from 'discord.js';
 import { Client as PGClient } from 'pg';
+import format from 'pg-format';
 
 export async function writeWeeklyXpSnapshots(client: PGClient, snapshots: Record<Snowflake, number>): Promise<void> {
-    await client.query('INSERT INTO weekly_xp_snapshots VALUES %L ON CONFLICT (rsn) DO UPDATE SET xp = EXCLUDED.xp;', Object.entries(snapshots));
+    await client.query(format('INSERT INTO weekly_xp_snapshots VALUES %L ON CONFLICT (rsn) DO UPDATE SET xp = EXCLUDED.xp;', Object.entries(snapshots)));
 }
 
 export async function fetchWeeklyXpSnapshots(client: PGClient): Promise<Record<Snowflake, number>> {
