0e01c6e10ce5368647a12b3a948b75dcf3aa1ce3
David Vorona <davidavorona@gmail.com>
2024-09-19 17:49:18 -0700

    Add postgres table, commands for guild settings

--- src/command-handler.ts
+++ src/command-handler.ts
@@ -10,16 +10,19 @@ import {
 import { MultiLoggerLevel } from 'evanw555.js';
 import { INVALID_TEXT_CHANNEL, UNAUTHORIZED_USER, STATE_DISABLED, UNAUTHORIZED_ROLE } from './constants';
 import {
-    BuiltSlashCommand,
     SlashCommandName,
     CommandOption,
     CommandWithOptions,
     SlashCommand,
-    SlashCommandsType
+    SlashCommandsType,
+    Subcommand
 } from './types';
 
 import state from './instances/state';
 import logger from './instances/logger';
+
+type OptionSlashCommand = Omit<SlashCommandBuilder, 'addSubcommand' | 'addSubcommandGroup'>;
+type SubcommandSlashCommand = Omit<SlashCommandBuilder, 'addBooleanOption' | 'addUserOption' | 'addChannelOption' | 'addRoleOption' | 'addAttachmentOption' | 'addMentionableOption' | 'addStringOption' | 'addIntegerOption' | 'addNumberOption'>;
 
 class CommandHandler {
     commands: SlashCommandsType;
@@ -123,13 +126,13 @@ class CommandHandler {
             // If there are choices and autocomplete is not on, then add them to
             // the list of static choices
             if (!autocomplete) {
-                option = option.addChoices(optionInfo.choices);
+                option = option.addChoices(...optionInfo.choices);
             }
         }
         return option;
     };
 
-    static buildCommandOptions(builder: BuiltSlashCommand, options: CommandOption[]) {
+    static buildCommandOptions(builder: OptionSlashCommand, options: CommandOption[]) {
         options.forEach((optionInfo: CommandOption) => {
             if (optionInfo.type === ApplicationCommandOptionType.String) {
                 builder = builder.addStringOption((option) => CommandHandler.buildCommandOption(option, optionInfo));
@@ -143,6 +146,25 @@ class CommandHandler {
         });
         return builder;
     }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    static buildSubcommand = (builder: any, subcommandInfo: Subcommand) => {
+        let subcommand = builder
+            .setName(subcommandInfo.name)
+            .setDescription(subcommandInfo.description);
+        if (subcommandInfo.options) {
+            subcommand = CommandHandler.buildCommandOptions(subcommand, subcommandInfo.options);
+        }
+        return subcommand;
+    };
+
+    static buildSubcommands(builder: SubcommandSlashCommand, subcommands: Subcommand[]) {
+        subcommands.forEach((subcommandInfo: Subcommand) => {
+            builder = builder.addSubcommand((subcommand) => CommandHandler.buildSubcommand(subcommand, subcommandInfo));
+        });
+        return builder;
+    }
+
     /**
      * Flexible method that takes static slash command data and uses the key/value arguments
      * to filter the list.
@@ -171,7 +193,7 @@ class CommandHandler {
      */
     buildCommand(key: SlashCommandName): ApplicationCommandDataResolvable {
         const commandInfo = this.commands[key];
-        let command: BuiltSlashCommand = new SlashCommandBuilder()
+        let command: SlashCommandBuilder = new SlashCommandBuilder()
             .setName(key)
             .setDescription(commandInfo.text);
         // If command has the admin flag, set the command permissions to admin
@@ -180,7 +202,10 @@ class CommandHandler {
         }
         // Build the command options if they exist
         if (commandInfo.options) {
-            command = CommandHandler.buildCommandOptions(command, commandInfo.options);
+            return CommandHandler.buildCommandOptions(command, commandInfo.options);
+        }
+        if (commandInfo.subcommands) {
+            return CommandHandler.buildSubcommands(command, commandInfo.subcommands);
         }
         return command;
     }
